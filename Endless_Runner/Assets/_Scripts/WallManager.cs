d4ea7487018a2405657e267022aff9116122e85d
Henk Raamkozijn <koen5067@icloud.com>
2014-05-08 15:08:08 +0200

    Aanpassingen muur

--- Endless_Runner/Assets/_Scripts/WallManager.cs
+++ Endless_Runner/Assets/_Scripts/WallManager.cs
@@ -6,11 +6,11 @@ public class WallManager : MonoBehaviour
 {
 	public GameObject runnerRef;
 	public float distanceToRecycle = 20;
-	public int numberOfWalls = 4;
-	public int movement = -5;
+	//public int numberOfWalls = 0;
+	//public int movement = -5;
 	public List<GameObject> wall = new List<GameObject>();
 
-	public GameObject[] wallCollection;
+	//public GameObject[] wallCollection;
 
 	static int randomWall = Random.Range(1,4);
 
@@ -20,49 +20,67 @@ public class WallManager : MonoBehaviour
 
 
 
-		GameObject wallPrefab = Resources.Load ("Prefabs/Wall" + randomWall) as GameObject;
+
+
+
+
+
 		for (int i=0; i<numberOfWalls; i++) 
 		{
+			//wall.Add(Resources.Load ("Prefabs/Stage" + randomWall) as GameObject);
+
+			/*
 			GameObject b = Instantiate (wallPrefab, Vector3.zero, Quaternion.identity) as GameObject;
 			Vector3 tempPos = new Vector3();
 			if(wall.Count>0)
 			{
 				tempPos = wall[wall.Count-1].transform.position;
-				tempPos.x += b.transform.localScale.x*.5f*35;
+				tempPos.x += b.transform.localScale.x*.5f*60;
 			}
 			b.transform.position = tempPos;
 			b.transform.parent = gameObject.transform;
-			b.transform.parent.localPosition = new Vector3(30,12,20);
 			wall.Add(b);
+
 		}
 	}
 	// Update is called once per frame
 	void Update () {
 
-		randomWall = Random.Range (1, 4);
+	randomWall = Random.Range (0, wall.Count);
+
 		Debug.Log (randomWall);
-
-		transform.Translate (new Vector2 (movement * Time.deltaTime, 0));
-
 		if (wall [0].transform.position.x + distanceToRecycle < runnerRef.transform.position.x) 
 		{
 			GameObject b = wall[0];
 			wall.Remove(b);
 
+			  
+			GameObject wallPrefab = wall[randomWall];//Resources.Load ("Prefabs/Wall" + randomWall) as GameObject;
 
-			GameObject wallPrefab = Resources.Load ("Prefabs/Wall" + Random.value * 2 + 1) as GameObject;
 
-
-			Vector3 tempPos = new Vector3();
+			/*Vector3 tempPos = new Vector3();
 			if(wall.Count>0)
 			{
 				tempPos = wall[wall.Count-1].transform.position;
-				tempPos.x += b.transform.localScale.x*.5f*35;
+				tempPos.x += b.transform.localScale.x*.5f*60;
 			}
 			b.transform.position = tempPos;
 			b.transform.parent = gameObject.transform;
-			//b.transform.parent.localPosition = new Vector3(30,12,20);
 			wall.Add(b);
+			//randomWall = Random.Range (1, 4);*/
 		}
+
+
+		Vector3 tempPos = new Vector3();
+		if(wall.Count>0)
+		{
+			tempPos = wall[wall.Count-1].transform.position;
+			tempPos.x += b.transform.localScale.x*.5f*60;
+		}
+		wall[0].
+		b.transform.position = tempPos;
+		b.transform.parent = gameObject.transform;
+		wall.
+		//transform.Translate (new Vector2 (movement * Time.deltaTime, 0));
 	}
 }
